616f786f877a2bdc659a1fe3bc6dcbb43825d213
Tristan Villanueva <tan_vil@hotmail.com>
2015-10-20 13:20:34 -0400

    fixed delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,47 +3,88 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
-  def fact
-  end
+  # def fact
+  # end
 
   def profile
+    @user = User.find_by(id: session[:user_id])
+    # id = @user.id
   end
 
-# we make a new show definition, then we "puts" something text, then
-# @user line is defining the user.find and params id is calling the user with
-# that id.
+  # we make a new show definition, then we "puts" something text, then
+  # @user line is defining the user.find and params id is calling the user with
+  # that id.
+
+  ##### everytime we wnat a new fnction we make a new def and a new html.erb file
+
+  #references to "username" from login.html.erb
 
 
-##### everytime we wnat a new fnction we make a new def and a new html.erb file
+  def loginverify
+    @user = User.where(
+      name: params[:username], 
+      password: params[:password]
+      ).first 
+    if @user
+      session[:user_id] = @user.id
+      redirect_to '/profile'
+      flash[:notice] = "Oh yays"
 
+    else
+      redirect_to '/login?ERROR'
+      flash[:notice] = "Wrong Credentials"
+    end
+  end
+
+  def logout
+    session.clear
+    redirect_to '/users'
+  end
 
   def show
-  	puts "Calling users#show with #{params[:id]}"
-  	@user = User.find(params[:id])
+    # puts "Calling users#show with #{params[:id]}"
+    @user = User.find(params[:id])
   end
 
   def destroy
-  	@zimmer = User.find(params[:id])
-  	@zimmer.destroy
-  end
-
-  def person_params
-  	params.require(:user).permit(:name,:email,:password,:post)
+    # puts "Calling users#destroy with #{params[:id]}"
+    @user = User.find(session[:user_id])
+    @user.delete
+    if @user.destroy!
+      session[:user_id].destroy
+      redirect_to new_user_path
+      flash[:notice] = "Mucho delete"
+    else
+      flash[:notice] = "Oh noes"
+    end
   end
 
   def create
-  	# params.require(:user).permit!
   	User.create(params[:user])
+    binding.pry
   	redirect_to "/users"
   end
 
   def edit
-  	@user = User.find(params[:id])
+   @user = User.find(params[:id])
   end
 
-  def update
-  	@user = User.find(params[:id])
-  	@user.update(params[:user])
-  	redirect_to user_path @user
+ def update
+   @user = User.find(params[:id])
+   @user.update(params[:user])
+   redirect_to user_path
+ end
+
+  def new
+    @user = User.new
   end
+  # def current_user
+  #   if session[:user_id]
+  #     @current_user = User.find(session[:user_id])
+  #   end
+  # end
+
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,13 @@ Rails.application.routes.draw do
 
   get 'users/profile'
 
-  get 'users/new'
+  get '/login', to: 'users#login'
 
-  get 'users/edit'
+  get '/logout', to: 'users#logout'
+
+  post '/loginverify', to: 'users#loginverify'
+
+  get '/profile', to: 'users#profile'
 
 # we are going to get the back end data into the front end with this
 # as a start
